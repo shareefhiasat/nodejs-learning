398245361d2f6459d569dd70a5ae4978326b8b93
shareef <shareef@sajilni.com>
2020-12-14 11:36:44 +0300

    6-server write html int response

--- 6-server.js
+++ 6-server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // we can store it but we dont have to.
 const server = http.createServer((req, res) => {
@@ -9,13 +10,26 @@ const server = http.createServer((req, res) => {
     // res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Content-Type', 'text/html');
 
-    //write the response
-    res.write('<head><link rel="stylesheet" href="#></link></head>')
-    // res.write('hello, shareef.');
-    res.write('<h1>hello, shareef.</h1>');
+    //send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // res.write(data);
+            //or shorthand if you have only 1 thing to write
+            res.end(data);
+        }
+        res.end();
+    });
 
-    //close the response
-    res.end();
+    //VERY BASIC WAY
+    // //write the response
+    // res.write('<head><link rel="stylesheet" href="#></link></head>')
+    // // res.write('hello, shareef.');
+    // res.write('<h1>hello, shareef.</h1>');
+
+    // //close the response
+    // res.end();
 });
 
 //default is localhost, but we define it explicity here
