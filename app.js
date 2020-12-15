8e852dad27598c6f4ab431df48958bd46b359e16
shareef <shareef@sajilni.com>
2020-12-15 19:22:44 +0300

    Add 404 handling

--- app.js
+++ app.js
@@ -19,8 +19,11 @@ app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname });
 });
 
+//add redirect example
 // app.get('/about', (req, res) => { res.send('<p>about</p>') });
 app.get('/about', (req, res) => { res.sendFile('./views/about.html', { root: __dirname }) });
 
-//add redirect example
-app.get('/about-us', (req, res) => { res.redirect('/about') });
+//Manage notfound, iff we dont have match and reach this point redirect to 404 not found page!, so we put it at last
+app.use((req, res) => {
+    res.status(404).sendFile('./views/404.html', { root: __dirname });
+});
